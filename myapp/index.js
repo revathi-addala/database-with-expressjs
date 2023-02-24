c5ea2502b596c625e63f128e78140267905aba16
revathi <reethu.addala@gmail.com>
2023-02-24 15:17:43 +0000

    database-with-expressjs

---
+++ myapp/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const dbPath = path.join(__dirname, "goodreads.db");
+
+const app = express();
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
